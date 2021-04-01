22464628167ca2cacee0cc70145e0abc68e45171
NeroYuki <kingrfminecraft@gmail.com>
2021-04-01 14:02:54 +0700

    update

--- App.js
+++ App.js
@@ -3,17 +3,15 @@ const Wit = require('node-wit');
 
 const app = express();
 
-const client = new Wit({
-    accessToken: process.env.WIT_ACCESS_TOKEN,
-});
+// const client = new Wit({
+//     accessToken: process.env.WIT_ACCESS_TOKEN,
+// });
 
 const port = process.env.PORT || 5000;
 
 app.get('/home', (req, res) => {
     res.send("Hello world");
 });
-
-app.post('/chat')
     
 app.listen(port, () => {
     console.log(`Express running → PORT ${port}`);
