9cc080f331a04b7f8b556f109e684e6280bb3aab
NeroYuki <kingrfminecraft@gmail.com>
2021-04-01 13:54:33 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Wit = require('node-wit');
+
+const app = express();
+
+const client = new Wit({
+    accessToken: process.env.WIT_ACCESS_TOKEN,
+});
+
+const port = process.env.PORT || 5000;
+
+app.get('/home', (req, res) => {
+    res.send("Hello world");
+});
+
+app.post('/chat')
+    
+app.listen(port, () => {
+    console.log(`Express running → PORT ${port}`);
+});
